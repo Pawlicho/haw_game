f39e7918669760fcf5fdefdc8ae0baf42e309457
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-06-16 16:06:00 +0200

    Initializing VS project

---
+++ HazardAndWhispers/HazardAndWhispers/App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HazardAndWhispers.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
