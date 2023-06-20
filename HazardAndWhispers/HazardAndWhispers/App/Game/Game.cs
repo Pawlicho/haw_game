e58877cebb68d23c70c014d62fe2412fa532c11c
Pawel Tymoczko <pawel.tymoczko@gmail.com>
2023-06-20 23:04:42 +0200

    Game main IF skeleton

---
+++ HazardAndWhispers/HazardAndWhispers/App/Game/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HazardAndWhispers.App.Game
+{
+    internal class Game
+    {
+        private IGameState state;
+
+        public IGameState State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public void Run()
+        {
+            while(true) 
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine(state.Action(keyInfo.Key));
+            }
+        }
+    }
+}
---
+++ HazardAndWhispers/HazardAndWhispers/App/Game/IGameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HazardAndWhispers.App.Alive;
+
+namespace HazardAndWhispers.App.Game
+{
+    internal interface IGameState
+    {
+        public Game GameContext { get; set; }
+        public Hero GameHero { get; set; }
+
+        public string Action(ConsoleKey key);
+        public void Init();
+        public IGameState ChangeState(IGameState prevState);
+    }
+}
